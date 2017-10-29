2d5ec5415aa2d9930317b269b9c579c8d7597505
Igor Dobrovolsky <idobrovolsky.g@gmail.com>
2017-10-30 00:03:09 +0200

    Leave only postcards

--- web-dapp/src/assets/javascripts/show-alert.js
+++
@@ -1,3 +0,0 @@
-window.show_alert = function (type, title, body) {
-    return window.swal(title, body, type);
-}
--- web-dapp/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { Footer } from './Footer';
-export { Header } from './Header';
-export { RegisterAddress } from './RegisterAddress'
-export { ConfirmAddress } from './ConfirmAddress'
