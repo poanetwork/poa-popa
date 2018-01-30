c22ec9ea67a1aa3fed67e167e435a7d58b84876f
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-01-30 10:44:33 -0300

    Move log_request method to file

---
+++ web-dapp/utils/log_request.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const logger = require('./logger');
+
+module.exports = (req, res, next) => {
+  req.log_prfx = `ID=${req.x_id}`;
+  if (req.x_ip) req.log_prfx += `|IP=${req.x_ip}`;
+
+  const logPrfx = req.log_prfx;
+  const method = req.method;
+  const path = req.path;
+  const headers = JSON.stringify(req.headers);
+  logger.log(`[request] (${logPrfx}) ${method} ${path} by ${headers}`);
+  next();
+};
