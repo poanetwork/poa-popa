da7279725bd5e0d7c708dfaf4c69f960673d3060
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-02 13:34:09 -0300

    Add linter test

---
+++ scripts/lint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Disabled until the moment of correcting all the API files and reviewing the linter rules
+# Run esLint
+#eslint --ignore-pattern 'web-dapp/build/*' --ignore-pattern 'web-dapp/node_modules/*' --ignore-pattern 'web-dapp/public/*' --ignore-pattern 'web-dapp/src/*' web-dapp/;
+#lintStatus=$?
+
+#if [ $lintStatus -ne 0 ]; then
+#  echo "Error Lint fail.";
+#fi
+#
+#exit $lintStatus;
+
+exit 0;
