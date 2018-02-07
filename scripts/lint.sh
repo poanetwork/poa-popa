8e097ba38b846a35f2504af0b3d687c9e134d4f2
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-07 10:49:13 -0300

    Update linter script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 # Disabled until the moment of correcting all the API files and reviewing the linter rules
 # Run esLint
-#eslint --ignore-pattern 'web-dapp/build/*' --ignore-pattern 'web-dapp/node_modules/*' --ignore-pattern 'web-dapp/public/*' --ignore-pattern 'web-dapp/src/*' web-dapp/;
-#lintStatus=$?
+eslint -c web-dapp/.eslintrc.yml --ignore-path web-dapp/.eslintignore web-dapp/;
+lintStatus=$?
 
-#if [ $lintStatus -ne 0 ]; then
-#  echo "Error Lint fail.";
-#fi
-#
-#exit $lintStatus;
+if [ $lintStatus -ne 0 ]; then
+  echo "Error Lint fail.";
+fi
 
-exit 0;
+exit $lintStatus;
