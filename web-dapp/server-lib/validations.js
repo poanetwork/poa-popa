65b44c8e4b23ea9dc3aec576f9d2a1446ab59f8b
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-23 03:16:11 -0300

    Simplify validation

--- web-dapp/server-lib/validations.js
+++ web-dapp/server-lib/validations.js
@@ -2,51 +2,31 @@
 const config = require('../server-config');
 const web3 = config.web3;
 
+
 const isString = (str) => {
-    if (typeof str !== 'string') {
-        return {
-            ok: false,
-            msg: `incorrect type, expecting string, but got ${typeof str}`,
-        };
-    }
-    return {ok: true};
+    return (typeof str === 'string');
 };
 
 const isWallet = (wallet) => {
-    if (!web3.isAddress(wallet)) {
-        return {
-            ok: false,
-            msg: 'not a valid hex-encoded ethereum address',
-        };
-    }
+    return (web3.isAddress(wallet));
+};
+
+const isEmpty = (str) => {
+    return (str.trim() === '');
+};
+
+const validString = (str) => {
+    if (!isString(str)) return {ok: false, msg: `incorrect type, expecting string, but got ${typeof str}`};
+    if (isEmpty(str)) return {ok: false, msg: 'empty'};
     return {ok: true};
 };
 
-const isNotEmpty = (str) => {
-    str = str.trim();
-    if (str === '') {
-        return {
-            ok: false,
-            msg: 'empty',
-        };
-    }
+const validWallet = (wallet) => {
+    if (!isString(wallet)) return {ok: false, msg: `incorrect type, expecting string, but got ${typeof wallet}`};
+    if (isEmpty(wallet)) return {ok: false, msg: 'empty'};
+    if (!isWallet(wallet)) return {ok: false, msg: 'not a valid hex-encoded ethereum address'};
     return {ok: true};
 };
-
-// Only for prepare_reg_tx and prepare_con_tx
-const oldString = (str) => {
-    if (typeof str !== 'string') return `incorrect type, expecting string, but got ${typeof str}`;
-    str = str.trim();
-    if (str === '') return 'empty';
-    return '';
-}
-const oldWallet = (str) => {
-    if (typeof str !== 'string') return `incorrect type, expecting string, but got ${typeof str}`;
-    str = str.trim();
-    if (str === '') return 'empty';
-    if (!web3.isAddress(str)) return 'not a valid hex-encoded ethereum address';
-    return '';
-}
 
 // these functions assume that their arguments have already been validated:
 const normalizeString = (str) => {
@@ -55,13 +35,8 @@ const normalizeString = (str) => {
 
 module.exports = {
     validate: {
-        wallet: isWallet,
-        string: isString,
-        notEmpty: isNotEmpty,
-        old: {
-            string: oldString,
-            wallet: oldWallet,
-        },
+        wallet: validWallet,
+        string: validString,
     },
     normalize: {
         string: normalizeString,
