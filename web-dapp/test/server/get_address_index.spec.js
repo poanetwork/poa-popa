9fd61db4f4f275cf67d0818eeea3957a62cba0c0
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-27 16:04:12 -0300

    Add test to get_address_index

---
+++ web-dapp/test/server/get_address_index.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+const {wallets, mockUserAddressByCreationBlock} = require('./_utils/mocks');
+const [wallet] = wallets
+jest.mock('../../server-config', () => ({contract: {user_address_by_creation_block: jest.fn(mockUserAddressByCreationBlock)}}));
+const getAddressIndex = require('../../server-lib/get_address_index');
+
+describe('Get Address Index', () => {
+    it('should return the user address index', () => {
+        const opts = {
+            wallet,
+            tx_bn: 10,
+        };
+        expect(getAddressIndex(opts)).resolves.toBeTruthy();
+    });
+});
