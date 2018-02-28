65a93303b54318dd0c8238db895af066a76b4453
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-28 14:05:27 -0300

    Use Winston as logger

--- web-dapp/server-lib/logger.js
+++ web-dapp/server-lib/logger.js
@@ -1,10 +1,30 @@
+'use strict';
+const winston = require('winston');
+
+const transports = [];
+
+if (process.env.NODE_ENV !== 'test') {
+    // Console transport
+    const consoleTransport = new winston.transports.Console({
+        level: process.env.LOG_LEVEL || 'debug',
+        colorize: true,
+        handleExceptions: true,
+        humanReadableUnhandledException: true
+    });
+    transports.push(consoleTransport);
+}
+
+const logger = new winston.Logger({
+    transports: transports
+});
+
 module.exports = {
     log: (msg) => {
         let logDate = new Date().toISOString();
-        console.log(logDate, msg);
+        logger.info(logDate, msg);
     },
     error: (msg) => {
         let logDate = new Date().toISOString();
-        console.error(logDate, msg);
+        logger.error(logDate, msg);
     },
 };
