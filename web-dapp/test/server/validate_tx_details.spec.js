d61bc85c578eeadee5cb2073bdf85fcc3535cbe3
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-02-28 11:17:03 -0300

    Add test to validate_tx_details

---
+++ web-dapp/test/server/validate_tx_details.spec.js
@@ -0,0 +1,69 @@
+'use strict';
+const [walletA, walletB, walletC] = require('./_utils/mocks').wallets;
+const [tx_id] = require('./_utils/mocks').txIds;
+const validateTxDetails = require('../../server-lib/validate_tx_details');
+
+describe('Validate transaction details', () => {
+    it('should reject if there is some error', () => {
+        const error = {error: 'Error!'};
+        const txDetails = null;
+        const contractAddress = null;
+        const wallet = null;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).rejects.toBeTruthy();
+    });
+    it('should reject if txDetails does not exist', () => {
+        const error = null;
+        const txDetails = null;
+        const contractAddress = null;
+        const wallet = null;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).rejects.toBeTruthy();
+    });
+    it('should reject txDetails.to is different to contractAddress', () => {
+        const error = null;
+        const txDetails = {
+            hash: tx_id,
+            to: walletA,
+            from: walletC,
+            blockNumber: 10,
+        };
+        const contractAddress = walletB;
+        const wallet = walletC;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).rejects.toBeTruthy();
+    });
+    it('should reject if txDetails.from is different to wallet', () => {
+        const error = null;
+        const txDetails = {
+            hash: tx_id,
+            to: walletB,
+            from: walletC,
+            blockNumber: 10,
+        };
+        const contractAddress = walletB;
+        const wallet = walletA;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).rejects.toBeTruthy();
+    });
+    it('should reject if txDetails.blocknumber does not exist', () => {
+        const error = null;
+        const txDetails = {
+            hash: tx_id,
+            to: walletB,
+            from: walletA,
+            blockNumber: null,
+        };
+        const contractAddress = walletB;
+        const wallet = walletA;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).rejects.toBeTruthy();
+    });
+    it('should resolves transaction details', () => {
+        const error = null;
+        const txDetails = {
+            hash: tx_id,
+            to: walletB,
+            from: walletA,
+            blockNumber: 10,
+        };
+        const contractAddress = walletB;
+        const wallet = walletA;
+        expect(validateTxDetails(error, txDetails, contractAddress, wallet)).resolves.toBeTruthy();
+    });
+});
