65f23748f8d319795122389f57790600701e6739
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-03-01 15:38:18 -0300

    Add tests to prepareRegTx

---
+++ web-dapp/test/server/prepareRegTx.spec.js
@@ -0,0 +1,149 @@
+'use strict';
+const {wallets, badWallets} = require('./_utils/mocks');
+const [wallet] = wallets;
+const [badWallet] = badWallets;
+const prepareRegTx = require('../../controllers/prepareRegTx');
+
+describe('Prepare Reg Transaction', () => {
+    describe('Validate Data', () => {
+        it('should reject if data is empty', () => {
+            expect(prepareRegTx.validateData()).rejects.toBeTruthy();
+        });
+        it('should reject if wallet is not valid', () => {
+            const data = {
+                wallet: badWallet,
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if name is not valid', () => {
+            const data = {
+                wallet,
+                name: '',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if country is not valid', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: '',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if state is not valid', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: 'us',
+                state: '',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if city is not valid', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: '',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if address is not valid', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should reject if zip is not valid', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '',
+            };
+            expect(prepareRegTx.validateData(data)).rejects.toBeTruthy();
+        });
+        it('should return wallet and params', () => {
+            const data = {
+                wallet,
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+            expect(prepareRegTx.validateData(data)).resolves.toBeTruthy();
+            return prepareRegTx.validateData(data)
+                .then(({wallet, params}) => {
+                    expect(wallet).toEqual(data.wallet);
+                    expect(params.name).toEqual(data.name.trim().toLowerCase());
+                    expect(params.country).toEqual(data.country.trim().toLowerCase());
+                    expect(params.state).toEqual(data.state.trim().toLowerCase());
+                    expect(params.city).toEqual(data.city.trim().toLowerCase());
+                    expect(params.address).toEqual(data.address.trim().toLowerCase());
+                    expect(params.zip).toEqual(data.zip.trim().toLowerCase());
+                    expect(params).not.toHaveProperty('wallet');
+                });
+        });
+    });
+
+    describe('Sign', () => {
+        it('should return the sign output, the price wei and the confirmation code (sha3 and plain)', () => {
+            const params = {
+                name: 'John Doe',
+                country: 'us',
+                state: 'ca',
+                city: 'Los Angeles',
+                address: '1219 Diane Street',
+                zip: '90013',
+            };
+
+            expect(prepareRegTx.sign(params, wallet)).resolves.toBeTruthy();
+            return prepareRegTx.sign(params, wallet)
+                .then(result => {
+                    expect(result).toHaveProperty('confirmationCodePlain');
+                    expect(result).toHaveProperty('sha3cc');
+                    expect(result).toHaveProperty('priceWei');
+                    expect(result).toHaveProperty('signOutput');
+                });
+        });
+    });
+
+    describe('Set Session Key', () => {
+        it('should return the session key', () => {
+            const confirmationCodePlain = 'sxxsndac7y7';
+            expect(prepareRegTx.setSessionKey((wallet, confirmationCodePlain))).resolves.toBeTruthy();
+        });
+    });
+});
