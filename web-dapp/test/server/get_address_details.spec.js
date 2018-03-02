b1811fa8cc1f221427349675bf6c9240ca0786b9
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-03-02 15:19:13 -0300

    Fix get_address_details test

--- web-dapp/test/server/get_address_details.spec.js
+++ web-dapp/test/server/get_address_details.spec.js
@@ -1,5 +1,13 @@
 'use strict';
 const [wallet] = require('./_utils/mocks').wallets;
+const mockUserAddress = ['us', 'ca', 'los angeles', 'Dennis Drive', '90017'];
+const mockUserAddressInfo = ['john doe'];
+jest.mock('../../server-config', () => ({
+    contract: {
+        user_address: jest.fn((a, b, cb) => cb(null,mockUserAddress)),
+        user_address_info: jest.fn((a, b, cb) => cb(null, mockUserAddressInfo)),
+    },
+}));
 const getAddressDetails = require('../../server-lib/get_address_details');
 
 describe('Get Address Details', () => {
