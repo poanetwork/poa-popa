defbfb23ddac4e81fafac5719c33bbc0afd7178b
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-03-14 15:51:59 -0300

    Remove Lob API Key for Travis

--- web-dapp/test/server/_utils/server-config-private.js
+++ web-dapp/test/server/_utils/server-config-private.js
@@ -1,7 +1,5 @@
 module.exports = function (cfg_public) {
     return {
-        lob_api_key: 'test_7867cccb176862a812b2bee4cf672431046',
-        lob_api_version: '2018-03-01',
         signer: '0xdbde11e51b9fcc9c455de9af89729cf37d835156',
         signer_private_key: '1dd9083e16e190fa5413f87837025556063c546bf16e38cc53fd5d018a3acfbb',
     };
