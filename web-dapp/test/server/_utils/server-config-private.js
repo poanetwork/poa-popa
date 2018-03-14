fa59ad66c13cf2a6fbdffe6070d7ac97c7a3dbc5
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-03-14 14:43:13 -0300

    Lob api key for Travis

--- web-dapp/test/server/_utils/server-config-private.js
+++ web-dapp/test/server/_utils/server-config-private.js
@@ -1,7 +1,7 @@
 module.exports = function (cfg_public) {
     return {
-        lob_api_key: 'test_e7fa8ab327a8e58193d0ca66c30a3ad8deb',
-        lob_api_version: '2017-06-16',
+        lob_api_key: 'test_7867cccb176862a812b2bee4cf672431046',
+        lob_api_version: '2018-03-01g',
         signer: '0xdbde11e51b9fcc9c455de9af89729cf37d835156',
         signer_private_key: '1dd9083e16e190fa5413f87837025556063c546bf16e38cc53fd5d018a3acfbb',
     };
