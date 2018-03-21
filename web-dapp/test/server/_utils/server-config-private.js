4da0c0f226ee8db4210fd1cda1e8767a9ce2cf27
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-03-21 08:45:10 -0300

    Fix test/signer-private-config

--- web-dapp/test/server/_utils/server-config-private.js
+++ web-dapp/test/server/_utils/server-config-private.js
@@ -1,6 +1,6 @@
 module.exports = function (cfg_public) {
     return {
         signer: '0xdbde11e51b9fcc9c455de9af89729cf37d835156',
-        signer_private_key: '1dd9083e16e190fa5413f87837025556063c546bf16e38cc53fd5d018a3acfbb',
+        signerPrivateKey: '1dd9083e16e190fa5413f87837025556063c546bf16e38cc53fd5d018a3acfbb',
     };
 };
