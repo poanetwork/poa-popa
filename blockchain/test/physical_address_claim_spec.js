c55a7dd93c8fd65a847d5415b43cf6cf80155eb0
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-02 16:15:14 -0300

    Add tests to PhysicalAddressClaim

---
+++ blockchain/test/physical_address_claim_spec.js
@@ -0,0 +1,68 @@
+const PhysicalAddressClaim = artifacts.require('PhysicalAddressClaim');
+
+contract('PhysicalAddressClaim', () => {
+    it.only('Should encodes the confirmation block number in a bytes32', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _confirmationBlockNumber = '0x0000000000000000000000000000000000000000000000000000000000FAFAFA';
+        const expected = '0x0100000000000000000000000000000000000000000000000000000000fafafa';
+        assert.equal(
+            await contract.encode(_confirmationBlockNumber),
+            expected
+        );
+    });
+
+    it.only('Should not encode if the confirmation block number is bigger than the Confirmation mask', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _confirmationBlockNumber = '0x0100000000000000000000000000000000000000000000000000000000FAFAFA';
+
+        await contract.encode(_confirmationBlockNumber)
+            .then(
+                () => assert.fail(), // should reject
+                () => {}
+            );
+    });
+
+    it.only('Should decodes the claim', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _claim = '0x0100000000000000000000000000000000000000000000000000000000FAFAFA';
+        const [version, confirmation] = await contract.decode(_claim);
+        assert.equal(
+            version,
+            1
+        );
+        assert.equal(
+            (confirmation).toFixed(),
+            parseInt('0x0000000000000000000000000000000000000000000000000000000000FAFAFA', 16)
+        );
+    });
+
+    it.only('Should not decode the claim if it is not the current version', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _claim = '0x0200000000000000000000000000000000000000000000000000000000FAFAFA';
+        await contract.decode(_claim)
+            .then(
+                () => assert.fail(), // should reject
+                () => {}
+            );
+    });
+
+    it.only('Should extracts the version', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _claim = '0x0100000000000000000000000000000000000000000000000000000000FAFAFA';
+        const expected = 1;
+        assert.equal(
+            (await contract.decodeVersion(_claim)).toFixed(),
+            expected
+        );
+    });
+
+    it.only('Should extracts the confirmation block number', async () => {
+        const contract = await PhysicalAddressClaim.deployed();
+        const _claim = '0x0100000000000000000000000000000000000000000000000000000000FAFAFA';
+        const expected = parseInt('0x0000000000000000000000000000000000000000000000000000000000FAFAFA', 16);
+        assert.equal(
+            (await contract.decodeConfirmation(_claim)).toFixed(),
+            expected
+        );
+    });
+});
