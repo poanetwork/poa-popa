56dce17bb04245d7520bf8d997d3ca3c318cd4ee
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-21 16:14:58 -0300

    Add the new design

---
+++ web-dapp/src/components/IndexPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class IndexPage extends React.Component {
+    render () {
+        return (
+            <div className="content">
+                <div className="col-md-12">
+                    <h1 className="main-title">Proof of Physical Address</h1>
+                    <p className="main-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris.</p>
+                    <button className="primary-btn mt-3">
+                        How it works
+                        <img className="btn-arrow" src={require('../assets/images/how-it-works.svg')} alt="how-it-works" />
+                    </button>
+                    <Link to="/register">
+                        <button className="action-btn mt-3">
+                            Continue
+                            <img className="btn-arrow" src={require('../assets/images/arrow.svg')} alt="arrow" />
+                        </button>
+                    </Link>
+                </div>
+                <div className="col-md-12">
+                    <h4 className="second-title">
+                        Lorem ipsum dolor sit amet
+                    </h4>
+                    <p className="second-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <Link to="/confirm">
+                        <button className="primary-btn mt-3">
+                            Verify postcard
+                            <img className="btn-arrow" src={require('../assets/images/verify.svg')} alt="verify" />
+                        </button>
+                    </Link>
+                    <Link to="/my-addresses">
+                        <button className="primary-btn mt-3">
+                            My addresses
+                            <img className="btn-arrow" src={require('../assets/images/my-addresses.svg')} alt="my-addresses" />
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
