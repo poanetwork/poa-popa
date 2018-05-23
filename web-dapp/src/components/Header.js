b15beebb42bd7da8a1ae4b92984949bbdc01ca53
fernandomg <fernando.greco@gmail.com>
2018-05-23 16:29:19 -0300

    Remove unnecessary a tag

--- web-dapp/src/components/Header.js
+++ web-dapp/src/components/Header.js
@@ -4,10 +4,8 @@ import { Link } from 'react-router-dom';
 class Header extends React.Component {
     render () {
         return (
-            <div className="col-md-12">
-                <Link to="/">
-                    <a className="logo" href="/"></a>
-                </Link>
+            <div className="col-md-12 header">
+                <Link to="/" className="logo"></Link>
             </div>
         );
     }
