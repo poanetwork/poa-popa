08117841118324ef8b1da9bbfc85c584056bcad2
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-05-23 08:28:42 -0300

    Fix link in Logo

--- web-dapp/src/components/Header.js
+++ web-dapp/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
     render () {
         return (
             <div className="col-md-12">
-                <a className="logo" href="index.html"></a>
+                <Link to="/">
+                    <a className="logo" href="/"></a>
+                </Link>
             </div>
         );
     }
