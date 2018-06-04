075d743ceab8a91c2360b507391e90dcef060ea4
phahulin <p.hahulin@hintsolutions.ru>
2018-06-04 12:48:53 +0300

    Add limits to post body size

--- web-dapp/app.js
+++ web-dapp/app.js
@@ -16,9 +16,10 @@ app.use('/confirm', express.static(path.join(__dirname, 'build')));
 app.use('/help', express.static(path.join(__dirname, 'build')));
 app.use('/my-addresses', express.static(path.join(__dirname, 'build')));
 
+const BODY_SIZE_LIMIT = '3kb';
 // api
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: BODY_SIZE_LIMIT }));
+app.use(bodyParser.json({ limit: BODY_SIZE_LIMIT }));
 
 const routes = require('./routes')({});
 app.use('/api', routes);
