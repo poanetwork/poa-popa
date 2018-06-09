f701fc393cea63678c7a973d26ee26e8b2083b7f
phahulin <p.hahulin@hintsolutions.ru>
2018-06-09 22:03:33 +0300

    Improve logging

--- web-dapp/server-lib/logger.js
+++ web-dapp/server-lib/logger.js
@@ -24,6 +24,7 @@ module.exports = {
     },
     error: (msg) => {
         let logDate = new Date().toISOString();
+        logger.info(logDate, msg);
         logger.error(logDate, msg);
     },
 };
