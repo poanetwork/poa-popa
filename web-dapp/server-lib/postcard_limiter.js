029811f97bac8d4a16eff1561c014a7bea1b2f5b
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2018-06-11 16:41:03 -0300

    Add petty-cache to fix issue when it use multiple processes

--- web-dapp/server-lib/postcard_limiter.js
+++ web-dapp/server-lib/postcard_limiter.js
@@ -1,27 +1,38 @@
 'use strict';
 
-const db = require('./session_store');
+const PettyCache = require('petty-cache');
+const pettyCache = new PettyCache();
+const db = require("./session_store");
 
 const MAX_POSTCARDS_PER_DAY = Number(process.env.MAX_POSTCARDS_PER_DAY || 10);
 
 function canSend() {
-    return get()
-        .then(postcardsSent => postcardsSent < MAX_POSTCARDS_PER_DAY);
+    return get().then(postcardsSent => postcardsSent < MAX_POSTCARDS_PER_DAY);
 }
 
 function get() {
     const postcardsSentKey = getDaysAfterEpoch();
-
-    return db.get(postcardsSentKey).then(x => {
-        if (x) return Number(x);
-        return 0;
+    const config = {
+        retry: { interval: 100, times: 5 },
+        ttl: 1000,
+    };
+    pettyCache.mutex.lock('postcardsSendMutex', config, err => {
+        if (err) {
+            //
+        }
+        return db.get(postcardsSentKey).then(x => {
+            if (x) return Number(x);
+            return 0;
+        });
     });
 }
 
 function inc() {
     const postcardsSentKey = getDaysAfterEpoch();
 
-    return db.inc(postcardsSentKey);
+    return db
+        .inc(postcardsSentKey)
+        .then(() => pettyCache.mutex.unlock('postcardsSendMutex'));
 }
 
 function getDaysAfterEpoch() {
