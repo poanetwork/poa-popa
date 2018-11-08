e86222231ddf95a0b66cdb0fe61a9ada74938d51
Jorge Luis Shirai <jlshirai@gmail.com>
2018-11-08 13:37:10 -0300

    Remove unused font-awesome dependency.

--- web-dapp/src/index.js
+++ web-dapp/src/index.js
@@ -7,7 +7,5 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './assets/stylesheets/application.css';
 
-import 'font-awesome/css/font-awesome.min.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
